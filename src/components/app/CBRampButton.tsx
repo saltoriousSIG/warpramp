7fa801362070785c58b06ec2adbcf62369b5be96
Sigala Fosam <cryptobet95@gmail.com>
2025-05-05 01:06:11 -0400

    ok

--- src/components/app/CBRampButton.tsx
+++ src/components/app/CBRampButton.tsx
@@ -66,8 +66,9 @@ const CBRampButton: React.FC<CBRampButtonProps> = ({
     )
 
     return (
-        <Button
+        <Button id="cbonramp-button-container"
             className="h-14 w-full bg-gradient-to-r from-violet-600 to-purple-600 text-base font-medium shadow-md transition-all hover:from-violet-700 hover:to-purple-700 hover:cursor-pointer"
+            onClick={handleOnPress}
             disabled={!isReady}>
             <CoinbaseIcon />
             Buy with Coinbase
