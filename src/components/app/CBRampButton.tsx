9cb8f60da8fe6b62fd3fd4a37efa2125aedec81a
Sigala Fosam <cryptobet95@gmail.com>
2025-05-05 03:21:27 -0400

    ok

--- src/components/app/CBRampButton.tsx
+++ src/components/app/CBRampButton.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
-import { initOnRamp } from '@coinbase/cbpay-js';
+import React, { useState, useEffect, useCallback } from "react";
+import { generateOnRampURL } from '@coinbase/cbpay-js';
 import { Button } from "../ui/button";
 import { CoinbaseIcon } from "../core/icons";
-import { toast } from "sonner"
-
+import { sdk } from "@farcaster/frame-sdk";
+import { toast } from "sonner";
 interface CBRampButtonProps {
     destinationWalletAddress: string;
     transferAmount: number;
@@ -13,75 +13,35 @@ const CBRampButton: React.FC<CBRampButtonProps> = ({
     destinationWalletAddress,
     transferAmount
 }) => {
-    const [mobileDevice, setMobileDevice] = useState<boolean>();
-    const [isReady, setIsReady] = useState(false);
-    const onrampInstance = useRef<any>();
+    //const [isReady, setIsReady] = useState(false);
+    const [rampUrl, setRampUrl] = useState<string>();
 
-    useEffect(() => {
-        const userAgent = navigator.userAgent;
-        const isMobile = /Mobi|Android|iPhone|iPad/.test(userAgent);
-        setMobileDevice(isMobile);
-    }, [])
 
     useEffect(() => {
         const options = {
             appId: (import.meta as any).env.VITE_CB_APP_ID, // Obtained from Coinbase Developer Platform
-            target: '#cbonramp-button-container',
-            widgetParameters: {
-                addresses: { [destinationWalletAddress]: ['base'], "0xAB51Bc7aa8636328E91bCC1B6bf701998fD3C581": ['base'] },
-                presetFiatAmount: transferAmount, // Prefill 100 USD
-                sourceCurrency: 'USD', // Fiat currency
-                assets: ["ETH"],
-                defaultNetwork: 'base', //
-            },
-            onSuccess: () => {
-                if (mobileDevice) {
-                    window.location.href = "https://warpramp-ztqy.vercel.app/"
-                } else {
-                    toast("Your purchase was successful")
-                }
-            },
-            onExit: (error: any) => {
-                console.error('Onramp exited:', error);
-            },
-            onEvent: (event: any) => {
-                console.log('Onramp event:', event);
-            },
-            experienceLoggedIn: 'embedded' as any,
-            experienceLoggedOut: 'popup' as any,
-            closeOnSuccess: true
+            addresses: { [destinationWalletAddress]: ['base'] },
+            assets: ['ETH'],
+            handlingRequestedUrls: true,
+            presetFiatAmount: transferAmount,
+            redirect_url: "https://warpramp-ztqy.vercel.app/"
         };
-
-        if (onrampInstance.current) {
-            onrampInstance.current.destroy();
-        }
-
-        initOnRamp(options, (error, instance) => {
-            if (error) return
-            if (instance) {
-                onrampInstance.current = instance;
-                setIsReady(true);
-            }
-        });
-
-        return () => {
-            if (onrampInstance.current) {
-                onrampInstance.current.destroy();
-            }
-        };
-    }, [destinationWalletAddress, transferAmount, mobileDevice]);
+        const onrampUrl = generateOnRampURL(options);
+        setRampUrl(onrampUrl);
+    }, [destinationWalletAddress, transferAmount]);
 
     const handleOnPress = useCallback(
         () => {
-            onrampInstance.current.open();
-        }, [transferAmount]
+            if (!transferAmount || isNaN(transferAmount)) return toast("Please enter an amount!")
+            if (rampUrl) sdk.actions.openUrl(rampUrl)
+        }, [rampUrl, transferAmount]
     )
 
     return (
         <Button id="cbonramp-button-container"
             className="h-14 w-full bg-gradient-to-r from-violet-600 to-purple-600 text-base font-medium shadow-md transition-all hover:from-violet-700 hover:to-purple-700 hover:cursor-pointer"
             onClick={handleOnPress}
-            disabled={!isReady}>
+        >
             <CoinbaseIcon />
             Buy with Coinbase
         </Button>
