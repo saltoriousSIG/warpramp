db7a916bd3c28faa81347cf6d50c90b17d508d83
Sigala Fosam <cryptobet95@gmail.com>
2025-05-07 00:21:49 -0400

    ok

--- src/components/app/CBRampButton.tsx
+++ src/components/app/CBRampButton.tsx
@@ -45,7 +45,7 @@ const CBRampButton: React.FC<CBRampButtonProps> = ({
                 console.log('Onramp event:', event);
             },
             experienceLoggedIn: "embedded" as any,
-            experienceLoggedOut: 'popup' as any,
+            experienceLoggedOut: "embedded" as any,
             closeOnSuccess: true,
             closeOnExit: true
         };
