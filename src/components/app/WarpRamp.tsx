ca74e88c5b43eee1aad73f26ef2f6a2977c5ca95
Sigala Fosam <cryptobet95@gmail.com>
2025-05-16 17:03:14 -0400

    ok

--- src/components/app/WarpRamp.tsx
+++ src/components/app/WarpRamp.tsx
@@ -40,7 +40,7 @@ const WarpRamp: React.FC<WarpRampProps> = ({ }) => {
             } = data;
             console.log(currency, recipients, start)
             sdk.actions.composeCast({
-                text: `I just got funded via Warp Ramp`,
+                text: `I just got funded via Warp Ramp @saltorious.eth`,
                 embeds: ["https://warpramp.link"]
             })
         }
